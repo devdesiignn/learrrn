ec270afe26e3d670ee2ff89ed91d7c32f4e2fe9b
DEVDESIIGNN <muizharuna55@gmail.com>
2024-07-30 17:00:41 +0100

    feat: authentication with clerk

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div className="h-full flex justify-center items-center">{children}</div>;
+}
