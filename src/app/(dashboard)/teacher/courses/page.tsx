05884bd7f70e276f6e05a1c14f899ae240930841
DEVDESIIGNN <muizharuna55@gmail.com>
2024-08-06 18:32:49 +0100

    feat: added course creation UI

--- src/app/(dashboard)/teacher/courses/page.tsx
+++ src/app/(dashboard)/teacher/courses/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+import Link from "next/link";
+
 export default function CoursesPage() {
-  return <div>Courses Page</div>;
+  return (
+    <div className="p-6">
+      <Link href={"/teacher/create"}>
+        <Button>Add New Course</Button>
+      </Link>
+    </div>
+  );
 }
